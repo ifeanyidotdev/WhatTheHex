55ef6cf101a61b9c3281116bb63904d6a6e1eeaa
Ifeanyichukwu Sampson Ebenezer <40169444+ifeanyidotdev@users.noreply.github.com>
2025-01-07 09:06:01 +0100

    changes container to cupertino icon

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,12 @@ class WhatTheHexScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Container(
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+                child: Container(
                   decoration: BoxDecoration(
                     color: Colors.grey.shade300,
                     borderRadius: BorderRadius.circular(10),
@@ -68,189 +69,202 @@ class WhatTheHexScreen extends StatelessWidget {
                     ],
                   ),
                 ),
-                const SizedBox(
-                  height: 80,
-                ),
-                // Container(
-                //   decoration: const BoxDecoration(
-                //     color: blueColor,
-                //     borderRadius: BorderRadius.all(
-                //       Radius.circular(50),
-                //     ),
-                //   ),
-                //   padding: const EdgeInsets.symmetric(horizontal: 12),
-                //   child: const Text(
-                //     "#",
-                //     style: TextStyle(
-                //       color: Colors.white,
-                //       fontSize: 32,
-                //       fontWeight: FontWeight.bold,
-                //     ),
-                //   ),
-                // ),
-                const Icon(
-                  CupertinoIcons.number_circle_fill,
-                  color: blueColor,
-                  size: 60,
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  "WhatTheHex?!",
-                  style: TextStyle(
-                    color: blackColor,
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  decoration: BoxDecoration(
-                    // color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "#227BFE",
-                      border: OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.circular(8), // Rounded corners
-                        borderSide: const BorderSide(
-                          color: blackColor, // Border color
-                          width: 1.5, // Border width
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(),
+              const SizedBox(
+                height: 40,
+              ),
+              // Container(
+              //   decoration: const BoxDecoration(
+              //     color: blueColor,
+              //     borderRadius: BorderRadius.all(
+              //       Radius.circular(50),
+              //     ),
+              //   ),
+              //   padding: const EdgeInsets.symmetric(horizontal: 12),
+              //   child: const Text(
+              //     "#",
+              //     style: TextStyle(
+              //       color: Colors.white,
+              //       fontSize: 32,
+              //       fontWeight: FontWeight.bold,
+              //     ),
+              //   ),
+              // ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  children: [
+                    const Icon(
+                      CupertinoIcons.number_circle_fill,
+                      color: blueColor,
+                      size: 60,
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      "WhatTheHex?!",
+                      style: TextStyle(
+                        color: blackColor,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(
+                        // color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "#227BFE",
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.circular(8), // Rounded corners
+                            borderSide: const BorderSide(
+                              color: blackColor, // Border color
+                              width: 1.5, // Border width
+                            ),
+                          ),
+                          hintStyle: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                          suffixIcon: const Icon(
+                            Icons.directions,
+                            color: blackColor,
+                          ),
+                        ),
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
                         ),
                       ),
-                      hintStyle: const TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                      ),
-                      suffixIcon: const Icon(
-                        Icons.directions,
-                        color: blackColor,
-                      ),
                     ),
-                    style: const TextStyle(
-                      fontSize: 18,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Row(
-                    children: [
-                      Icon(
-                        CupertinoIcons.ellipses_bubble_fill,
-                        color: blackColor,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text("Type or pick your color and press enter ")
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 30),
-                Material(
-                  borderRadius: BorderRadius.circular(8),
-                  elevation: 3,
-                  child: Container(
-                    height: 290,
-                    width: 375,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                    const SizedBox(height: 10),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Row(
                         children: [
-                          Row(
-                            children: [
-                              Container(
-                                height: 65,
-                                width: 65,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: blueColor,
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              const Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Shadow Blue',
-                                    style: TextStyle(
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    "Closest Name",
-                                    style: TextStyle(fontSize: 12),
-                                  )
-                                ],
-                              )
-                            ],
+                          Icon(
+                            CupertinoIcons.exclamationmark_bubble_fill,
+                            color: blackColor,
                           ),
-                          const SizedBox(height: 15),
-                          _colorOptionns(
-                              title: "RGBA", content: "rgba(77, 81, 101, 1)"),
-                          const SizedBox(height: 10),
-                          _colorOptionns(
-                              title: "CSS",
-                              content: "--color-clear-blue: #227BFE;"),
-                          const SizedBox(height: 10),
-                          _colorOptionns(
-                              title: "SCSS",
-                              content: "\$color-clear-blue: #227BFE;"),
-                          const SizedBox(height: 20),
-                          Container(
-                            width: 340,
-                            height: 40,
-                            decoration: BoxDecoration(
-                              color: blackColor,
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: const Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  CupertinoIcons.square_on_square,
-                                  color: Colors.white,
-                                ),
-                                SizedBox(
-                                  width: 5,
-                                ),
-                                Text(
-                                  "GENERATE SHADES",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                )
-                              ],
-                            ),
-                          )
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text("Type or pick your color and press enter ")
                         ],
                       ),
                     ),
-                  ),
+                    const SizedBox(height: 30),
+                    Material(
+                      borderRadius: BorderRadius.circular(8),
+                      elevation: 3,
+                      child: Container(
+                        height: 290,
+                        width: 375,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    height: 65,
+                                    width: 65,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      color: blueColor,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  const Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Shadow Blue',
+                                        style: TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        "Closest Name",
+                                        style: TextStyle(fontSize: 12),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                              const SizedBox(height: 15),
+                              _colorOptionns(
+                                  title: "RGBA",
+                                  content: "rgba(77, 81, 101, 1)"),
+                              const SizedBox(height: 10),
+                              _colorOptionns(
+                                  title: "CSS",
+                                  content: "--color-clear-blue: #227BFE;"),
+                              const SizedBox(height: 10),
+                              _colorOptionns(
+                                  title: "SCSS",
+                                  content: "\$color-clear-blue: #227BFE;"),
+                              const SizedBox(height: 20),
+                              Container(
+                                width: 340,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: blackColor,
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: const Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      CupertinoIcons.square_on_square,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      "GENERATE SHADES",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    const Text(
+                      "Made with 🖤 by Kasun Peiris",
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 50),
-                const Text(
-                  "Made with 🖤 by Kasun Peiris",
-                  style: TextStyle(
-                    fontSize: 14,
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
